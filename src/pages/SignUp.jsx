68d6049e88cc3883f58a67619167233b6f95f601
Alejandro <alejandro.delgado.garcia.1992@gmail.com>
2025-05-14 12:38:22 +0200

    Modified Create Account style

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -27,8 +27,8 @@ function SignUp() {
     const { data, error } = await supabase.auth.signUp({
       email,
       password,
-      options: { 
-        data: { name }
+      options: {
+        data: { name },
       },
     });
     setLoading(false);
@@ -39,47 +39,70 @@ function SignUp() {
       return;
     }
 
-    setMessage("Account created successfully! Check your email for confirmation.");
+    setMessage(
+      "Account created successfully! Check your email for confirmation."
+    );
 
     setTimeout(() => navigate("/"), 4000);
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Your name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <br /><br />
-        <input
-          type="email"
-          name="email"
-          placeholder="youremail@site.com"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <br /><br />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <br /><br />
+      <form
+        onSubmit={handleSubmit}
+        className="bg-[#fff] border-2 border-[#EAEBED] dark:border-gray-700 rounded-lg p-4 max-w-md mx-auto mt-10 dark:bg-gradient-to-r from-slate-900 to-slate-800"
+      >
+        <h1 className="text-4xl mb-4 dark:text-white text-left">Sign in</h1>
+        <div className="mb-8 text-gray-500 text-left">
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-black dark:border-gray-900 dark:placeholder-gray-700 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+
+        <div className="mb-8 text-gray-500 text-left">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="youremail@site.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-black dark:border-gray-900 dark:placeholder-gray-700 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+
+        <div className="mb-8 text-gray-500 text-left">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-black dark:border-gray-900 dark:placeholder-gray-700 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+
         {message && (
-          <p style={{ color: message.includes("successfully") ? "green" : "red", marginTop: "10px" }}>
+          <p
+            style={{
+              color: message.includes("successfully") ? "green" : "red",
+              marginTop: "10px",
+            }}
+          >
             {message}
           </p>
         )}
-        <button type="submit" disabled={loading}>
+        <button className="text-white dark:text-black bg-black dark:bg-white hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2  dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="submit" disabled={loading}>
           {loading ? "Creating account..." : "Join"}
         </button>
       </form>
